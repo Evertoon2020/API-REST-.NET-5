a88ac4a9020a91407206b3dfbd799c5b87bd9190
Everton Santos <everton-r.santos@hotmail.com>
2022-09-11 23:13:23 -0300

    Relacionamento 1:n

---
+++ src/FilmesAPI/FilmesAPI/Profiles/GerenteProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.Dtos.Gerente;
+using FilmesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Profiles
+{
+    public class GerenteProfile : Profile
+    {
+        public GerenteProfile()
+        {
+            CreateMap<CreateGerenteDto, Gerente>();
+            CreateMap<Gerente, ReadGerenteDto>()
+                .ForMember(x => x.Cinemas, opts => opts
+                .MapFrom(x => x.Cinemas.Select
+                (x => new { x.Id, x.Nome, x.Endereco, x.EnderecoId })));
+        }
+    }
+}
